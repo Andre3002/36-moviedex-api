f59c6ed2a189c82fed2c8b3b4bb07731cd38da66
Andre <afowlk1@gmail.com>
2020-11-09 20:06:08 -0500

    working api key

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const MOVIEDEX = require('./movies-data-small.json')
+
+console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
 
-// app.use((req, res) => {
-//     res.send('Hello, Andre!')
-// })
+app.use(function validateBearerToken(req,res,next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken){
+        return res.status(401).json({error:'Unauthorized request'})
+    }
+    // move to next piece of middleware
+    next()
+})
 
 app.get('/movie', function handleGetMovie(req, res) {
     let response = MOVIEDEX
@@ -37,9 +47,8 @@ app.get('/movie', function handleGetMovie(req, res) {
     res.json(response)
 })
 
-
 const PORT = 8000
 
 app.listen(PORT, () => {
-    console.log('Server listening at http://localhost:${PORT}')
+    console.log(`Server listening at http://localhost:${PORT}`)
 })
